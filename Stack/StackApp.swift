1eb3ba5ca410dd4f0d5a7629cc63a905c7fce907
isaiasegonzalez <xisaiasgonzalez@gmail.com>
2025-10-29 14:16:56 -0500

    Initial Commit

---
+++ Stack/StackApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackApp.swift
+//  Stack
+//
+//  Created by Isaias Gonzalez on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct StackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
